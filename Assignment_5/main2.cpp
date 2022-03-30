fa5bccf982d860be35f4f653c12a528f7b0f0c42
Splitsky <opop.neska60@gmail.com>
2022-03-30 21:47:46 +0100

    main2 is the main working version of the code. Copy constructor and = operator overload are finished. Check that the other overloaded operators work and streamline the << and >> operation to make sure that there are no data leaks

--- Assignment_5/main2.cpp
+++ Assignment_5/main2.cpp
@@ -11,15 +11,6 @@
 #include<array>
 #include<iterator>
 #include<cstring>
-// Things to do
-// The class:
-// 1. Contain no. of rows, no. of columns and a pointer to the matrix data (type double)
-// 2. Constructor using dynamic memory allocation
-// 3. Store data in a one dimensional array. For a matrix by m x n. The location is given by (j-1) + (i-1)*n
-// 4. the destructor should delete dynamically allocated memory
-// 5. Assignment operator and copy constructor functions would perform deep copies of data
-// 6. Recursive calculation of a determinant:
-// 7. 
 
 class matrix {
     friend std::ostream& operator<<(std::ostream &os, matrix &numbers);
@@ -33,35 +24,49 @@ class matrix {
         matrix();
         matrix(int row, int col, double data_in[]);
         ~matrix(){
+            std::cout << "calling destructor" << std::endl;
             this->data = nullptr;
             delete[] this->data;
         }
 
-        matrix(const matrix&);// copy constructor
+        matrix(matrix&);// copy constructor
         void set_row(int row);
         void set_col(int col);
         int get_row() const;
         int get_col() const;
-        void set_data(const double* data_2, size_t size);
+        void set_data(double* data_2, size_t size);
+        // overloading operators
         matrix operator+(matrix &numbers) const;
         matrix operator-(matrix &numbers) const;
         matrix operator*(matrix &numbers) const;
         matrix& operator=(matrix &numbers);
         std::string show_data() const;
-    
+        double* get_data() const;
+
 };
 matrix::matrix() {
-            this->data = nullptr;
-            this->row = 0;
-            this->col = 0;
-        }
-        // parameter constructor
+    this->data = nullptr;
+    this->row = 0;
+    this->col = 0;
+}
+
 matrix::matrix(int row, int col, double data_in[]): row{row}, col{col} {
-            this->data = new double[row*col]; // declares memory space
-            for (int i{0}; i<row*col; i++) { // performs deep copy
-                this->data[i] = data_in[i];
-            }
-        }
+    this->data = new double[row*col]; // declares memory space
+    for (int i{0}; i<row*col; i++) { // performs deep copy
+          this->data[i] = data_in[i];
+    }
+}
+
+matrix::matrix(matrix& numbers) { // COPY CONSTRUCTOR
+    std::cout << "Copy constructor called" << std::endl;
+    this->set_col(numbers.get_col());
+    this->set_row(numbers.get_row());
+    this->set_data(numbers.get_data(), numbers.get_col()*numbers.get_row());
+}
+
+double* matrix::get_data() const {
+    return this->data;
+}
 
 int matrix::get_row() const{
     return this->row;
@@ -75,14 +80,16 @@ void matrix::set_row(int row){
 void matrix::set_col(int col){
     this->col=col;
 }
-void matrix::set_data(const double* data_2, size_t size){
+void matrix::set_data(double* data_2, size_t size){
+    // this function perfroms a deep copy of the data taking care of memory leaks
+    //if (this->data != nullptr) {
+    //    delete[] this->data;
+    //}
     this->data = new double[size];
     for (size_t i{0}; i< size; i++) {
         this->data[i] = data_2[i];
-        std::cout << this->data[i] << std::endl;
-        // copies data from one pointer location to the other
     }
-    return;        
+    return;
 }
 
 matrix matrix::operator+(matrix &numbers) const {
@@ -155,20 +162,19 @@ matrix matrix::operator*(matrix &numbers) const {
     }
 }
 
-matrix& matrix::operator=(matrix &numbers) { // deep copy
+matrix & matrix::operator=(matrix &numbers) { // deep copy
+    std::cout << "Calling assignment operator" << std::endl;
     if (&numbers == this) {return *this;} // avoids self assignment    
-        delete[] data; 
-        data = nullptr;
-        col = 0;row = 0;
-        col = numbers.col;
-        row = numbers.row;
-        if (row*col>0) {
-            data = new double[row*col];
-            for (size_t i{}; i<col*row; i++) {
-                data[i]=numbers.data[i];
-            }
-        }
-        return *this;
+    delete[] this->data;
+    
+    this->data = nullptr;
+    this->col = numbers.col;
+    this->row = numbers.row;
+    
+    if (col*row != 0) {
+        this->set_data(numbers.get_data(), this->col*this->row);
+    }        
+    return *this;
 }
 
 std::string matrix::show_data() const {
@@ -184,14 +190,15 @@ std::string matrix::show_data() const {
                 temp_string += "], ";
             }
             temp_string = temp_string.substr(0,temp_string.size()-2);
-
         return temp_string;
 }
-
 
 std::ostream& operator<<(std::ostream& os, matrix &numbers) {
     // this function prints matrix in visually representative way
     os << "Printing the matrix:" << std::endl;
+    std::cout << numbers.get_row() << " , " << numbers.get_col() << " and stuff" << std::endl;
+    std::cout << numbers.get_data()[0] << numbers.get_data()[1] << std::endl;
+
     std::string temp[numbers.get_row()];
     std::string temp_string;
     for (int i{1}; i <= numbers.get_row(); i++) {
@@ -269,22 +276,14 @@ int main() {
     pointer[2] = 5;
     pointer[3] = 5;
     matrix D(2,2,pointer);
-    delete[] pointer; // frees up assignmed memory in the main
+    std::cout << D;
 
-    matrix F; // demonstrates the overloaded functions
-    std::cin >> F;
-    std::cout << "One" << std::endl;
+    matrix G;
+    matrix F = D; // copy constructor
+    G = D; // assignment operator
     std::cout << F;
-    std::cout << D;
+    std::cout << G;
 
-
-    D = F;
-    std::cout << "Two" << std::endl;
-    std::cout << D; 
-    D.set_data(pointer, 4);
-    std::cout << "Three" << std::endl;
-    std::cout << D;
-
-
+    delete[] pointer;
     return 0;
 }  
